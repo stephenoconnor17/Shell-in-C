92def181c49403b9a3e67bd7a16dcf8e12a0dc3c
steph_oconnor <g00467249@atu.ie>
2026-07-06 15:54:10 +0100

    added null checks on mallocs

--- tokeniser.c
+++ tokeniser.c
@@ -2,16 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int is_delim(char c) {
+static int is_delim(char c) {
     return c == ' ' || c == '\n' || c == '\t'; 
+}
+
+void free_tokens(char** args){
+    int i = 0;
+    while(*(args + i) != NULL){
+        free(*(args + i));
+        i++;
+    }
+    free(args);
 }
 
 char** tokenise(char* line){
     int onWord = 0;
     int wordCount = 0;
-
     int i = 0;
     char current = *(line + i);
+
     while(current != '\0'){
         if(!is_delim(current) && onWord == 0){
             wordCount++;
@@ -27,34 +36,40 @@ char** tokenise(char* line){
     //an dynamic array of dynamic strings.
     //+1 for null to acknowledge end of arr.
     char** arr = malloc(sizeof(char*) * (wordCount + 1));
+    if(arr == NULL) return NULL;
 
+    //reset for second pass.
     i = 0;
     onWord = 0;
     current = *(line + i);
     int j = 0;
-
-    int currentWordLength = 0;
+    int wordLength = 0;
 
     do{
         if(!is_delim(current) && onWord == 0){
             onWord = 1;
-            currentWordLength++;
+            wordLength++;
         }else if((is_delim(current) || current == '\0') && onWord == 1){
             onWord = 0;
-            *(arr + j) = malloc(sizeof(char) * (currentWordLength + 1));
-            for(int k = 0; k < currentWordLength; k++){
-                *(*(arr + j)+k) = *(line + i - currentWordLength + k);
+            if((*(arr + j) = malloc(sizeof(char) * (wordLength + 1))) == NULL){
+                free_tokens(arr);
+                return NULL;
             }
-            //h e l p e r
-            //0 1 2 3 4 5
-            //6
-            *(*(arr + j)+currentWordLength) = '\0';
+            for(int k = 0; k < wordLength; k++){
+                *(*(arr + j)+k) = *(line + i - wordLength + k);
+                //jth string, kth char assigned 
+                //the line's first character up to i minus length of word + k
+            }
+            
+            //because each string must end in a \0
+            *(*(arr + j)+wordLength) = '\0';
             j++;
-            currentWordLength = 0;
+            wordLength = 0;
         }else if(!is_delim(current)){
-            currentWordLength++;
+            wordLength++;
         }
         
+        //to make sure we dont read past end of line.
         if(j < wordCount){
             i++;
             current = *(line + i);
@@ -66,11 +81,3 @@ char** tokenise(char* line){
     return arr;
 }
 
-void free_tokens(char** args){
-    int i = 0;
-    while(*(args + i) != NULL){
-        free(*(args + i));
-        i++;
-    }
-    free(args);
-}
--- tokeniser.h
+++ tokeniser.h
@@ -1,6 +1,5 @@
 #ifndef TOKENISER_H
 #define TOKENISER_H
-int is_delim(char c);
 char** tokenise(char* line);
 void free_tokens(char** args);
 #endif
